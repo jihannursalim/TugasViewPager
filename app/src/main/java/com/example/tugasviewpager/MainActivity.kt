cde406137a3466ac983c02dd1aff723ccb1a8f3a
nisanurainisalim <ainisyarifsuhandari@gmail.com>
2023-04-30 00:50:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tugasviewpager/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.tugasviewpager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+
+class MainActivity : AppCompatActivity() {
+
+    // on below line we are creating variable for view pager,
+    // viewpager adapter and the image list.
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var imageList: List<Int>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // initializing variables
+        // of below line with their id.
+        viewPager = findViewById(R.id.idViewPager)
+
+        // on below line we are initializing
+        // our image list and adding data to it.
+        imageList = ArrayList<Int>()
+        imageList = imageList + R.drawable.android
+        imageList = imageList + R.drawable.c
+        imageList = imageList + R.drawable.java
+        imageList = imageList + R.drawable.js
+        imageList = imageList + R.drawable.img
+
+        // on below line we are initializing our view
+        // pager adapter and adding image list to it.
+        viewPagerAdapter = ViewPagerAdapter(this@MainActivity, imageList)
+
+        // on below line we are setting
+        // adapter to our view pager.
+        viewPager.adapter = viewPagerAdapter
+    }
+}
